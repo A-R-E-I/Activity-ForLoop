1e2c983681134f0f4a108a71b5f464c695b655e5
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2024-10-21 21:20:34 -0400

    Add files via upload

--- ForLoopJava.js
+++ ForLoopJava.js
@@ -1,3 +1,40 @@
-/**
- * 
- */
+window.addEventListener("load",addListener);
+let Whatquestion = 0
+function addListener()
+{
+	document.getElementById("txtDisplay").disabled = true;
+	document.getElementById("btnAvg").addEventListener("click",QuestionSelect);
+}
+
+function QuestionSelect()
+{ 
+	if(Whatquestion < 2)
+	{
+		Whatquestion = Whatquestion + 1;
+	}
+	
+	switch(Whatquestion)
+	{
+		case 1:
+			GradeAmount = document.getElementById("txtInputnum").value;
+			document.getElementById("txtInputnum").value = " ";
+			document.getElementById("numofgrades").textContent = "Enter Grade:";
+			break;
+			
+		case 2:
+			let sum = 0
+			for(let i=0; i < GradeAmount; i++)
+				{	
+					
+					GradeInput = document.getElementById("txtInputnum").value;
+					sum = parseInt(sum) + parseInt(GradeInput);
+					GradeInput = 0;
+					document.getElementById("txtInputnum").value = "";
+					
+				}
+			document.getElementById("txtDisplay").value = sum;
+			break;
+	}
+	
+}
+
